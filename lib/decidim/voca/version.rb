1fc0c9781992da21d03d6a872e9863400b4d4054
Hadrien Froger <hadrien@octree.ch>
2025-07-31 11:53:21 +0100

    release: update version

--- lib/decidim/voca/version.rb
+++ lib/decidim/voca/version.rb
@@ -4,7 +4,7 @@ module Decidim
   # This holds the decidim-meetings version.
   module Voca
     def self.version
-      "0.0.4" # DO NOT UPDATE MANUALLY
+      "0.0.5" # DO NOT UPDATE MANUALLY
     end
 
     def self.decidim_version
