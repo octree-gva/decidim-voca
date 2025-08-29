8ef942e6a4e90198c9b722e787dbf0aa02ff2761
Hadrien Froger <hadrien@octree.ch>
2025-08-29 07:12:23 +0100

    feat: add language in header!

--- app/overrides/insert_weglot_scripts.rb
+++ app/overrides/insert_weglot_scripts.rb
@@ -15,10 +15,42 @@ Deface::Override.new(
   closing_selector: "erb[silent]:contains('end')",
   text: <<~ERB
     <% if ::Decidim::Voca.weglot? %>
-      <%= render partial: "decidim/voca/weglot/locale_switcher" %>
+      <%= render partial: "decidim/voca/weglot/locale_switcher", locals: { switcher_id: "desktop" } %>
     <% end %>
     <div class="voca-js--original-language-chooser" data-weglot-active="<%= ::Decidim::Voca.weglot? %>">
       <%= render_original %>
     </div>
+  ERB
+)
+# layouts/decidim/header/_mobile_language_chooser
+Deface::Override.new(
+  virtual_path: "layouts/decidim/header/_mobile_language_choose",
+  name: "insert_weglot_mobile_language_chooser",
+  surround: "erb[silent]:contains('available_locales.length > 1')",
+  closing_selector: "erb[silent]:contains('end')",
+  text: <<~ERB
+    <% if ::Decidim::Voca.weglot? %>
+      <%= render partial: "decidim/voca/weglot/locale_switcher_mobile", locals: { switcher_id: "mobile" } %>
+    <% end %>
+    <div class="voca-js--original-language-chooser" data-weglot-active="<%= ::Decidim::Voca.weglot? %>">
+      <%= render_original %>
+    </div>
+  ERB
+)
+
+Deface::Override.new(
+  virtual_path: "layouts/decidim/header/_main_links_desktop",
+  name: "insert_weglot_main_links_desktop_classes",
+  set_attributes: "div:has(erb[loud]:contains('decidim.pages_path'))",
+  attributes: {
+    class: "main-bar__links-desktop__item-wrapper"
+  }
+)
+Deface::Override.new(
+  virtual_path: "layouts/decidim/header/_main_links_desktop",
+  name: "insert_weglot_main_links_desktop",
+  insert_bottom: "div:has(erb[loud]:contains('decidim.pages_path'))",
+  text: <<~ERB
+    <%= render partial: "decidim/voca/weglot/topbar_locale_switcher" %>
   ERB
 )
--- app/packs/src/decidim/voca/weglot/switcher.js
+++ app/packs/src/decidim/voca/weglot/switcher.js
@@ -1,70 +1,87 @@
+function initializeCommentsDropdown(buttons) {
+  if(buttons.length === 0) {
+    return;
+  }
+  buttons.forEach((button) => {
+    button.addEventListener("click", function (event) {
+      event.preventDefault();
+      event.stopPropagation();
+      const target = button.getAttribute("data-target");
+      const dropdown = document.getElementById(target);
+      const wasHidden = dropdown.getAttribute("aria-hidden") === "true";
+      console.log("WAS HIDDEN", wasHidden);
+      dropdown.setAttribute("aria-hidden", !wasHidden);
+      // If not hidden and click outside, click on the button again
+      if (wasHidden) {
+        const closeDropdown = () => {
+          dropdown.setAttribute("aria-hidden", true);
+          document.removeEventListener("click", closeDropdown);
+        }
+        document.addEventListener("click", closeDropdown);
+      }
+    });
+  });
+}
+
 export default async function switcher() {
-  const searchContainer = document.querySelector(
-    ".main-footer__language-container.voca-js--weglot-locale-switcher"
+  const searchContainers = document.querySelectorAll(
+    ".vocajs_weglot__locale_switcher"
   );
-  if (!searchContainer || searchContainer.length === 0) {
+  if (!searchContainers || searchContainers.length === 0) {
     return;
   }
   const WeglotInstance = window.Weglot;
   if (!WeglotInstance) {
     return;
   }
-
-  const currentLang = WeglotInstance.getCurrentLang();
-  const currentLanguageName = WeglotInstance.getLanguageName(currentLang);
-  const button = searchContainer.querySelector(
-    ".main-footer__language-trigger"
-  );
-  button.addEventListener("click", function (event) {
-    event.preventDefault();
-    const dropdown = searchContainer.querySelector(
-      ".voca-js--weglot-locale-switcher-dropdown"
-    );
-    dropdown.setAttribute("aria-hidden", !dropdown.getAttribute("aria-hidden"));
-  });
-  const buttonContent = searchContainer.querySelector(
-    ".voca-js--weglot-current-language"
-  );
-  buttonContent.textContent = currentLanguageName;
-
-  const availableLanguages = WeglotInstance.options.languages
-    .filter(({ enabled }) => enabled)
-    .map(({ language_to }) => language_to)
-    .concat(WeglotInstance.options.language_from)
-    .filter((lang) => lang !== currentLang);
-
-  const selectList = searchContainer.querySelector(
-    ".voca-js--weglot-locale-switcher-dropdown .main-footer__language"
-  );
-  availableLanguages.forEach((lang) => {
-    const listItem = document.createElement("li");
-    listItem.classList.add("text-black", "text-md");
-    listItem.setAttribute("data-language", lang);
-
-    const textItem = document.createElement("span");
-    textItem.classList.add("p-2", "w-full", "block");
-    textItem.textContent = WeglotInstance.getLanguageName(lang);
-    listItem.append(textItem);
-
-    if (lang !== currentLang) {
-      listItem.classList.add(
-        "hover:bg-secondary",
-        "hover:text-white",
-        "transition"
+  const buttons = []
+   searchContainers.forEach((searchContainer) => {
+    console.log("searchContainer", searchContainer);
+    const currentLang = WeglotInstance.getCurrentLang();
+    const currentLanguageName = WeglotInstance.getLanguageName(currentLang);
+      const buttonContent = searchContainer.querySelector(
+        ".vocajs_weglot__locale_current"
       );
+    if(currentLang && currentLanguageName) {
+      buttonContent.textContent = currentLanguageName;
     }
+    const availableLanguages = (WeglotInstance.options.languages || [])
+      .filter(({ enabled }) => enabled)
+      .map(({ language_to }) => language_to)
+      .concat(WeglotInstance.options.language_from)
+      .filter((lang) => lang !== currentLang);
 
-    listItem.addEventListener("click", function (event) {
-      event.preventDefault();
+    if (availableLanguages.length > 0) {
+      const selectList = searchContainer.querySelector(
+        ".vocajs_weglot__locale_dropdown > ul"
+      );
+      const itemClasses = searchContainer.getAttribute("data-weglot-item-class").split(" ");
+      const textItemClasses = searchContainer.getAttribute("data-weglot-textitem-class").split(" ");
+      availableLanguages.forEach((lang) => {
+        const listItem = document.createElement("li");
+        listItem.classList.add(...itemClasses);
+        listItem.setAttribute("data-language", lang);
 
-      console.log("Voca Weglot clicked on", lang);
-      WeglotInstance.switchTo(lang);
-      buttonContent.textContent = WeglotInstance.getLanguageName(lang);
-    });
-    selectList.appendChild(listItem);
+        const textItem = document.createElement("span");
+        textItem.classList.add(...textItemClasses);
+        textItem.textContent = WeglotInstance.getLanguageName(lang);
+        listItem.append(textItem);
+
+        listItem.addEventListener("click", function (event) {
+          event.preventDefault();
+
+          console.log("Voca Weglot clicked on", lang);
+          WeglotInstance.switchTo(lang);
+          buttonContent.textContent = WeglotInstance.getLanguageName(lang);
+        });
+        selectList.appendChild(listItem);
+      });
+    }
+    buttons.push(searchContainer.querySelector("[data-controller='dropdown']"));
   });
-
+  initializeCommentsDropdown(buttons);
   WeglotInstance.on("languageChanged", function (lang) {
     console.log("Voca Weglot language changed to", lang);
   });
+  window.initFoundation(window.document);
 }
