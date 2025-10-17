a0983da1f10ef35cf03c973b2651ba75cbae27c4
Renato <renato@octree.ch>
2025-10-17 11:23:31 -0300

    code review

--- lib/decidim/voca/overrides/attachment_overrides.rb
+++ lib/decidim/voca/overrides/attachment_overrides.rb
@@ -10,7 +10,8 @@ module Decidim
           alias_method :decidim_file_type, :file_type
 
           def file_type
-            return file.blob.filename.extension_without_delimiter if file?
+            byebug
+            return file.blob.filename.extension_without_delimiter if file? && file&.blob&.filename
 
             "link" if link?
           end
