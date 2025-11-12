6b58287f474bb81b16aa4ceff4228e8298f1e93e
Renato <renato@octree.ch>
2025-11-12 10:58:46 -0300

    chore: add force_email=true to send e-mail the proposal's author and followers

---
+++ lib/decidim/voca/overrides/notify_proposal_answer_overrides.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Voca
+    module Overrides
+      # Add the param { force_email: true } to send an email to the proposal's author
+      # and followers, regardless of their notification email settings.
+      module NotifyProposalAnswerOverrides
+        extend ActiveSupport::Concern
+
+        included do
+          alias_method :decidim_original_notify_followers, :notify_followers
+
+          def notify_followers
+            byebug
+            return if proposal.state == "not_answered"
+
+            Decidim::EventsManager.publish(
+              event: "decidim.events.proposals.proposal_state_changed",
+              event_class: Decidim::Proposals::ProposalStateChangedEvent,
+              resource: proposal,
+              affected_users: proposal.notifiable_identities,
+              followers: proposal.followers - proposal.notifiable_identities,
+              extra: { force_email: true }
+            )
+          end
+        end
+      end
+    end
+  end
+end
