a193400c839eb6273b4f1ccedd47cd0acf5f6e41
Hadrien Froger <hadrien@octree.ch>
2025-12-17 11:20:10 +0000

    chore: register code census as ephemeral

--- lib/decidim/voca/code_census/workflow.rb
+++ lib/decidim/voca/code_census/workflow.rb
@@ -4,4 +4,6 @@ Decidim::Verifications.register_workflow(:code_census) do |workflow|
   workflow.engine = Decidim::Voca::CodeCensus::Engine
   workflow.admin_engine = Decidim::Voca::Admin::CodeCensus::AdminEngine
   workflow.icon = "community-line"
+  workflow.time_between_renewals = 1.day
+  workflow.ephemerable = true
 end
