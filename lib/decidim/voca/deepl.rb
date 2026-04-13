c28cd8d83760d972ea786d9600c6255d0e45c652
Hadrien Froger <hadrien@octree.ch>
2026-04-13 13:45:19 +0100

    refactor: improve code quality

--- lib/decidim/voca/deepl.rb
+++ lib/decidim/voca/deepl.rb
@@ -7,22 +7,3 @@ require_relative "deepl/deepl_machine_translator"
 require_relative "deepl/deepl_middleware"
 require_relative "deepl/translation_bar_overrides"
 require_relative "deepl/engine_config"
-
-# Compatibility shim: older code referenced +Decidim::Voca::Deepl+.
-module Decidim
-  module Voca
-    DeeplContext = DeepL::Context
-    DeeplMiddleware = DeepL::Middleware
-    DeeplMachineTranslator = DeepL::MachineTranslator
-    DeeplActiveJobContext = DeepL::ActiveJobContext
-    DeeplFormBuilderOverrides = DeepL::DeepLFormBuilderOverrides
-
-    module Deepl
-      def self.const_missing(name)
-        return Decidim::Voca::DeepL.const_get(name) if Decidim::Voca.const_defined?(:DeepL)
-
-        super
-      end
-    end
-  end
-end
