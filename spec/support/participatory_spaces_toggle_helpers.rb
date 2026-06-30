ee18d49550d71acbb4c8a0d9935c24b704fead59
Hadrien Froger <hadrien@octree.ch>
2026-06-30 16:32:24 +0100

    lint: fix rubocop

--- spec/support/participatory_spaces_toggle_helpers.rb
+++ spec/support/participatory_spaces_toggle_helpers.rb
@@ -13,7 +13,7 @@ module ParticipatorySpacesToggleHelpers
 
   def resolve_participatory_space_key(space_or_module)
     key = space_or_module.to_sym
-    return key if Decidim::Voca::ParticipatorySpaces::SPACES.key?(key)
+    return key if Decidim::Voca::ParticipatorySpaces::SPACES.has_key?(key)
 
     Decidim::Voca::ParticipatorySpaces.space_for_module_name(space_or_module) ||
       raise(ArgumentError, "Unknown participatory space: #{space_or_module}")
