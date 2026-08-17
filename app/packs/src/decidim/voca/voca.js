ec95f1aa7a12dbdbdaa23856cc086e496d09cfd8
Renato <renato@octree.ch>
2026-08-17 10:18:08 -0300

    chore: remove geolocated proposal improvements assets

--- app/packs/src/decidim/voca/voca.js
+++ app/packs/src/decidim/voca/voca.js
@@ -1,6 +1,4 @@
 import "./proposal_custom_fields/proposal_custom_fields";
-//import "./proposal_better_ux/map_reload";
-//import "./proposal_better_ux/find_my_location_button";
 import "./proposal_better_ux/use_camera_button";
 import "./weglot";
 import "./questionnaire/scroll_questionnaire_steps_to_top";
