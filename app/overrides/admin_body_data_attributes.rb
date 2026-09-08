ebe4027a9329729fe51c59d26133b5c23910fd20
Renato <renato@octree.ch>
2026-09-08 05:52:06 -0300

    remove data_attributes

--- app/overrides/admin_body_data_attributes.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-Deface::Override.new(
-  virtual_path: "layouts/decidim/admin/_application",
-  name: "voca_admin_body_data_attributes",
-  replace: "body",
-  text: "<%= render partial: \"decidim/voca/toggle/admin/application\" %>"
-)
--- app/overrides/participatory_spaces_public_body_classes.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-Deface::Override.new(
-  virtual_path: "layouts/decidim/_application",
-  name: "participatory_spaces_public_body_data_attributes",
-  replace: "body",
-  text: "<%= render partial: \"decidim/voca/toggle/application\" %>"
-)
